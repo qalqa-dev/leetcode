d88eb45019d64ffaa95cb803a71c12107c505da5
qalqa-dev <andreybas04@gmail.com>
2025-04-10 16:29:49 +0300

    fib

---
+++ fibGen.js
@@ -0,0 +1,58 @@
+// var fibGenerator = function* () {
+//   const sequence = [0, 1];
+//   yield sequence[0];
+//   yield sequence[1];
+//   while (true) {
+//     let lastTwoSum = sequence[0] + sequence[1];
+//     yield lastTwoSum;
+//     sequence.shift();
+//     sequence.push(lastTwoSum);
+//   }
+// };
+
+// const gen = fibGenerator();
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+let a = 1;
+let b = 2;
+[a, b] = [b, a + 1];
+console.log(a, b);
